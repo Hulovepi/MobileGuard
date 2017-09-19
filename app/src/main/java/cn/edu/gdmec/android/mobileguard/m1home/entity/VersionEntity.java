3a4adf6498dc0f7191f4542b1443e764baee5d73
Hulovepi <807697194@qq.com>
2017-09-19 12:35:27 +0800

    自动升级代码

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/entity/VersionEntity.java
@@ -0,0 +1,11 @@
+package cn.edu.gdmec.android.mobileguard.m1home.entity;
+
+/**
+ * Created by Administrator on 2017/9/18 0018.
+ */
+
+public class VersionEntity {
+    public String versioncode;
+    public String description;
+    public String apkurl;
+}
