659315cc51eab7639882ae04ab9eaa4b6b3be2ee
Hulovepi <807697194@qq.com>
2017-10-21 12:24:53 +0800

    2017.10.21 手机防盗界面SIM卡绑定和设置安全联系人的业务逻辑实现

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/entity/ContactInfo.java
@@ -0,0 +1,11 @@
+package cn.edu.gdmec.android.mobileguard.m2theftguard.entity;
+
+/**
+ * Created by Administrator on 2017/10/21 0021.
+ */
+
+public class ContactInfo {
+    public String id;
+    public String name;
+    public String phone;
+}
