bd4f129b998909ed6ec4bed6c0d8cf51d5de4dc6
Hulovepi <807697194@qq.com>
2017-11-19 23:44:21 +0800

    2017.11.15 第五章--手机杀毒   完成补坑

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m5virusscan/dao/AntiVirusDao.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m5virusscan/dao/AntiVirusDao.java
@@ -5,35 +5,41 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 /**
- * Created by Administrator on 2017/11/14 0014.
+ * Created by Administrator on 2017/11/16.
  */
 
 public class AntiVirusDao {
-    /**
-     * 检查某个md5是否是病毒
-     *@param md5
-     *@return null 代表扫描安全
-     */
     private static Context context;
     private static String dbname;
-    public AntiVirusDao(Context context) {
-        this.context = context;
-        dbname = "/data/data/" + context.getPackageName() + "/files/antivirus.db";
+    public AntiVirusDao(Context context){
+        this.context =context;
+        dbname ="/data/data/"+context.getPackageName()+"/files/antivirus.db";
     }
-    //使用apk文件的md5值匹配病毒数据库
-    public String checkVirus(String md5){
+    public String checkVirus(String md5) {
         String desc = null;
-        //打开病毒数据库
         SQLiteDatabase db = SQLiteDatabase.openDatabase(
-                dbname,null,
-        SQLiteDatabase.OPEN_READONLY);
+                dbname, null,
+                SQLiteDatabase.OPEN_READONLY);
         Cursor cursor = db.rawQuery("select desc from datable where md5=?",
-                new String[]{md5});
-        if (cursor.moveToNext()){
+                new String[] { md5 });
+        if (cursor.moveToNext()) {
             desc = cursor.getString(0);
         }
         cursor.close();
         db.close();
         return desc;
+    }
+    public String getVirusDbVersion(){
+        String dbVersion = null;
+        SQLiteDatabase db = SQLiteDatabase.openDatabase(
+                dbname, null,
+                SQLiteDatabase.OPEN_READONLY);
+        Cursor cursor = db.rawQuery("select major||'.'||minor||'.'||build from version",null);
+        if (cursor.moveToNext()) {
+            dbVersion = cursor.getString(0);
+        }
+        cursor.close();
+        db.close();
+        return dbVersion;
     }
 }
